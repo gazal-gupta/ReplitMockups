9cc88a27cb76ddd95853805f0ae25eda394394e8
Gazal Gupta <gazalgupta30@gmail.com>
2026-09-16 08:24:50 +0530

    Add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,1 @@
+import { StrictMode } from "react";\nimport { createRoot } from "react-dom/client";\nimport App from "./App";\nimport "./index.css";\n\ncreateRoot(document.getElementById("root")!).render(\n  <StrictMode>\n    <App />\n  </StrictMode>,\n);\n
